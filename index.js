54caa7aa7ca9bc69c4c4e14c156664853f274870
mawa <mawahama05@gmail.com>
2022-11-13 01:20:15 -0800

    completed

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Add your code here
+const formData = {
+    name: "Steve",
+    email: "steve@steve.com",
+  };
+ const submitData = (name,email) => {
+    const configurationObject = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+        body: JSON.stringify(formData)};
+        fetch("http://localhost:3000/users", configurationObject)
+        .then( (response) => response.json())
+          .then(data =>document.body.innerHTML = data.id)
+          .catch(error => document.body.innerHTML = error.message)
+          ;
+ }
+
+ 
